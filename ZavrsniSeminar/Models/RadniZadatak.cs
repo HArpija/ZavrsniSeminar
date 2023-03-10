7e127fb39c223d826d7407c19534bfa977f9a299
HArpija <Helga@DESKTOP-DAD59KM>
2023-03-10 15:07:01 +0100

    Add project files.

---
+++ ZavrsniSeminar/Models/RadniZadatak.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace ZavrsniSeminar.Models
+{
+    public class RadniZadatak
+    {
+        public int Id { get; set; }
+        public string Todo { get; set; }
+        public string RadnikIme { get; set; }
+        public string RadnikPrezime { get; set; }
+        public string OpisPosla { get; set; }
+        public DateTime Datum { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
